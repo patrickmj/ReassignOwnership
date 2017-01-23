387f220cdc71d3c924f1a31c92caf978b456dfde
patrickmj <patrickmjchnm@gmail.com>
2017-01-23 14:29:28 -0500

    add old changes

--- ReassignOwnershipPlugin.php
+++ ReassignOwnershipPlugin.php
@@ -25,7 +25,7 @@ class ReassignOwnershipPlugin extends Omeka_Plugin_AbstractPlugin
     public function hookAdminCollectionsPanelFields($args)
     {
         $this->_echoPanel($args);
-    }    
+    }
     
     public function hookBeforeSaveItem($args)
     {
@@ -35,14 +35,16 @@ class ReassignOwnershipPlugin extends Omeka_Plugin_AbstractPlugin
     public function hookBeforeSaveCollection($args)
     {
         $this->_reassignOwnership($args);
-    }    
+    }
     
     private function _reassignOwnership($args)
     {
-        $newOwnerId = $args['post']['reassign_ownership_id'];
-        $record = $args['record'];
-        $newOwner = $this->_db->getTable('User')->find($newOwnerId);
-        $record->setOwner($newOwner);
+        if (isset($args['post']['reassign_ownership_id'])) {
+            $newOwnerId = $args['post']['reassign_ownership_id'];
+            $record = $args['record'];
+            $newOwner = $this->_db->getTable('User')->find($newOwnerId);
+            $record->setOwner($newOwner);
+        }
     }
     
     private function _echoPanel($args)
@@ -50,11 +52,11 @@ class ReassignOwnershipPlugin extends Omeka_Plugin_AbstractPlugin
         $view = $args['view'];
         $record = $args['record'];
         $user = current_user();
-        if( ($user->id == $record->isOwnedBy($user)) || $user->role == 'admin' || $user->role == 'super' ) {
+        if( $record->exists() && ( ($user->id == $record->isOwnedBy($user)) || $user->role == 'admin' || $user->role == 'super' )) {
             echo "<div class='field'>";
             echo "<label for='reassign_ownership_id'>" . __('Reassign Ownership') .  "</label>";
             echo $view->formSelect('reassign_ownership_id', $record->getOwner()->id, array('id'=>'reassign-ownership'), get_table_options('User', null, array('sort_field' => 'name')));
             echo "</div>";
-        }        
+        }
     }
 }
