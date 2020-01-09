a5f7f9eed86464a107b1bbacb0cc4bdf2fb28ea9
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2020-01-09 09:45:23 +0100

    Added batch-edit capability, plus localization
    Added the option to reassign ownership during items batch-editing. Also added localization.

--- ReassignOwnershipPlugin.php
+++ ReassignOwnershipPlugin.php
@@ -3,11 +3,24 @@
 class ReassignOwnershipPlugin extends Omeka_Plugin_AbstractPlugin
 {
     protected $_hooks = array(
+			'initialize',
             'admin_items_panel_fields',
-            'before_save_item',
+			'admin_items_show_sidebar',
+            'admin_items_batch_edit_form',
             'admin_collections_panel_fields',
+			'admin_collections_show_sidebar',
+            'before_save_item',
+            'items_batch_edit_custom',
             'before_save_collection'
             );
+			
+	/**
+    * Add the translations.
+    */
+    public function hookInitialize()
+    {
+        add_translation_source(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'languages');
+    }
     
     public function setUp()
     {
@@ -19,18 +32,44 @@ class ReassignOwnershipPlugin extends Omeka_Plugin_AbstractPlugin
     
     public function hookAdminItemsPanelFields($args)
     {
-        $this->_echoPanel($args);
+        $this->_echoChangePanel($args);
+    }
+	
+	public function hookAdminItemsShowSidebar($args)
+    {
+        $this->_echoShowPanel($args);
+    }
+    
+    public function hookAdminItemsBatchEditForm($args)
+    {
+        $this->_echoBatchPanel($args);
     }
 
     public function hookAdminCollectionsPanelFields($args)
     {
-        $this->_echoPanel($args);
+        $this->_echoChangePanel($args);
     }
-    
+	
+	public function hookAdminCollectionsShowSidebar($args)
+    {
+        $this->_echoShowPanel($args);
+    }
+
     public function hookBeforeSaveItem($args)
     {
         $this->_reassignOwnership($args);
     }
+    
+    public function hookItemsBatchEditCustom($args)
+    {
+		if (isset($args['custom']['reassign_ownership_id']) && $args['custom']['reassign_ownership_id'] != '') {
+			$newOwnerId = $args['custom']['reassign_ownership_id'];
+			$item = $args['item'];
+			$newOwner = $this->_db->getTable('User')->find($newOwnerId);
+			$item->setOwner($newOwner);
+			$item->save();
+		}
+    }   
 
     public function hookBeforeSaveCollection($args)
     {
@@ -47,16 +86,43 @@ class ReassignOwnershipPlugin extends Omeka_Plugin_AbstractPlugin
         }
     }
     
-    private function _echoPanel($args)
+    private function _echoShowPanel($args)
+    {
+        $record = isset($args['item']) ? $args['item'] : $args['collection'];
+        $owner = $record->getOwner();
+        if ($owner) {
+            echo "<div class='info panel'>\n";
+            echo "<h4>". __('Owner'). "</h4>\n";
+			echo "<div><p>" . $owner->name . "</p></div>\n";
+			echo "</div>\n";
+        }
+    }
+
+    private function _echoChangePanel($args)
     {
         $view = $args['view'];
         $record = $args['record'];
         $user = current_user();
         if( $record->exists() && ( ($user->id == $record->isOwnedBy($user)) || $user->role == 'admin' || $user->role == 'super' )) {
-            echo "<div class='field'>";
-            echo "<label for='reassign_ownership_id'>" . __('Reassign Ownership') .  "</label>";
+            echo "<div id='reassign_ownership-form' class='field'>";
+            echo "<label for='reassign_ownership_id'>" . __('Owner') .  "</label>";
+            echo "<div class='inputs'>";
             echo $view->formSelect('reassign_ownership_id', $record->getOwner()->id, array('id'=>'reassign-ownership'), get_table_options('User', null, array('sort_field' => 'name')));
             echo "</div>";
+            echo "</div>";
         }
+    }
+
+    private function _echoBatchPanel($args)
+    {
+        $view = $args['view'];
+        echo "<div class='field'>";
+        echo "<label class='two columns alpha' for='custom[reassign_ownership_id]'>" . __('Owner') .  "</label>";
+        echo "<div class='inputs five columns omega'>";
+        $ownershipOptions = get_table_options('User', null, array('sort_field' => 'name'));
+        $ownershipOptions = array('' => __('Select Below')) + $ownershipOptions;
+        echo $view->formSelect('custom[reassign_ownership_id]', null, array(), $ownershipOptions);
+        echo "</div>";
+        echo "</div>";
     }
 }
